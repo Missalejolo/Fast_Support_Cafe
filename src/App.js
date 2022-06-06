f212312de391ada62b30632a9e626186012740f3
EnmanuelDominguez <denmanuel795@gmail.com>
2022-06-06 14:20:49 -0500

    Lista desplegable de ayudas de optimización

--- src/App.js
+++ src/App.js
@@ -4,9 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" >
         <div><img src={logo} className="App-logo" alt="logo"/></div>
         <div class="box2">Inicio</div>
+        <nav>
+          <ul id="menu">
+          <li><a href="">Ayudas de optimización</a>
+            <ul>
+              <li><a href="">Desfragmentación de disco</a></li>
+              <li><a href="">Example</a></li>
+              <li><a href="">Example</a></li>
+            </ul>  
+          </li>
+          </ul>
+        </nav>
         <div class="box3">Fast Support Café</div>
       </header>
     </div>
