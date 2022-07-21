dd19a283fe951979cfe4bfdf4cfa2df6d731dd6f
EnmanuelDominguez <denmanuel795@gmail.com>
2022-07-21 00:08:25 -0500

    Cambios header

--- src/pantallas/config.js
+++ src/pantallas/config.js
@@ -2,7 +2,15 @@
 import { createChatBotMessage } from "react-chatbot-kit";
 
 const config = {
-  initialMessages: [createChatBotMessage(`¡Hola! Bienvenido a Fast Support Cafe, espero que estés bien. ¿En qué podemos ayudarte?`)]
-}
+  initialMessages: [createChatBotMessage(`¡Hola! Bienvenido a Fast Support Cafe, espero que estés bien. ¿En que podemos ayudarte?`)],
+  botName: "Fast Support Cafe",
+  customStyles: {
+  },
+  state: {
+      movieTitles: ["Hola", "xD"]
+  }
+  }
+
+
 
 export default config
